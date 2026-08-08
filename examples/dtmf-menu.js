f091a32384b97b6cfcbd42b01c11b02179acfab1
Pyspoof <pyspoof@protonmail.com>
2026-08-09 00:13:08 +1000

    Delete examples/dtmf-menu.js

--- examples/dtmf-menu.js
+++
@@ -1,9 +0,0 @@
-import { VoipBotClient } from "../src/client.js";
-
-const client = new VoipBotClient();
-
-const keymap = await client.keymap();
-
-for (const [key, action] of Object.entries(keymap)) {
-  console.log(`${key} -> ${action}`);
-}
